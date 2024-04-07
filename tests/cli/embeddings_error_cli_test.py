5dd303ef43eb6e9d5578d6b88f48b1cc4e5aa6a2
Zheng Ye <dreamhead.cn@gmail.com>
2024-04-07 21:39:20 +0800

    added embeddings error test for cli

---
+++ tests/cli/embeddings_error_cli_test.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+
+import openai
+import pytest
+
+from mocogpt.cli.app import __file__ as app_file
+
+
+class TestMocoGPTCliError:
+    def test_should_run_with_rate_limit_response(self, client):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        config_file = os.path.join(current_directory, "embeddings_error_cli.json")
+        service_process = subprocess.Popen(
+            ["python", app_file,
+             "start", config_file,
+             "--port", "12306"
+             ])
+
+        with pytest.raises(openai.RateLimitError):
+            client.embeddings.create(
+                model="text-embedding-ada-002",
+                input="rate limit",
+                encoding_format="float"
+            )
+
+        service_process.terminate()
+        service_process.wait()
+
+    def test_should_run_with_authentication_error_response(self, client):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        config_file = os.path.join(current_directory, "embeddings_error_cli.json")
+        service_process = subprocess.Popen(
+            ["python", app_file,
+             "start", config_file,
+             "--port", "12306"
+             ])
+
+        with pytest.raises(openai.AuthenticationError):
+            client.embeddings.create(
+                model="text-embedding-ada-002",
+                input="authentication error",
+                encoding_format="float"
+            )
+
+        service_process.terminate()
+        service_process.wait()
