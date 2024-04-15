6357573a5763c7b3e7356b7409c736e4b5448948
Zheng Ye <dreamhead.cn@gmail.com>
2024-04-15 21:29:19 +0800

    upgraded aiohttp to 3.9.4

--- mocogpt/core/actual_server.py
+++ mocogpt/core/actual_server.py
@@ -100,6 +100,7 @@ class ActualGptServer(GptServer):
     async def _stop(self):
         await self.runner.cleanup()
 
+
     async def chat_completions_api(self, request: web.Request):
         json_request = await request.json()
         await self.monitor.on_session_start(json_request)
