fe04b48e0ff835bdacee7c69196b2dad138814da
Zheng Ye <dreamhead.cn@gmail.com>
2024-05-09 20:12:42 +0800

    added follow redirect test

--- tests/mocogpt_redirect_test.py
+++ tests/mocogpt_redirect_test.py
@@ -25,5 +25,21 @@ class TestMocoRedirect:
                     messages=[{"role": "user", "content": "Hi"}]
                 )
 
+    def test_should_follow_redirect_307(self, client: OpenAI):
+        client = OpenAI(base_url="http://localhost:12306/v1", api_key="sk-123456789",
+                        http_client=httpx.Client(base_url="http://localhost:12306/v1", follow_redirects=True))
 
+        server = gpt_server(12306)
+        server.chat.completions.request(prompt="Hi").response(
+            redirect=redirect(307, "http://localhost:12307/v1/chat/completions"))
 
+        next_server = gpt_server(12307)
+        next_server.chat.completions.request(prompt="Hi").response(content="How can I assist you?")
+
+        with server:
+            with next_server:
+                response = client.chat.completions.create(
+                    model="gpt-4",
+                    messages=[{"role": "user", "content": "Hi"}]
+                )
+                assert response.choices[0].message.content == "How can I assist you?"
