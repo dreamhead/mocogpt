c8bd1a6baf7422957855eae5f3977802824092c1
Zheng Ye <dreamhead.cn@gmail.com>
2024-07-08 21:30:26 +0800

    added messages cli test

--- tests/cli/chat_completions_cli_test.py
+++ tests/cli/chat_completions_cli_test.py
@@ -174,6 +174,18 @@ class TestMocoGPTCli:
 
             assert response.choices[0].message.content == "Hi, top_logprobs"
 
+    def test_should_reply_content_for_specified_messages(self, client: OpenAI):
+        with self.run_service("chat_completions_config.json", "12306"):
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo-1106",
+                messages=[
+                    {"role": "assistant", "content": "Hi, assistant"},
+                    {"role": "user", "content": "Hi, users"}
+                ]
+            )
+
+            assert response.choices[0].message.content == "Hi, messages"
+
     def test_should_run_with_organization_and_project(self, client):
         with self.run_service("chat_completions_config.json", "12306"):
             client = OpenAI(base_url="http://localhost:12306/v1", api_key="sk-123456789", organization="123456",
