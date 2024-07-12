adec11a7b0aab3abe8ddf0fa0b0eb09333774461
Zheng Ye <dreamhead.cn@gmail.com>
2024-07-12 22:16:20 +0800

    added tools cli test

--- tests/cli/chat_completions_cli_test.py
+++ tests/cli/chat_completions_cli_test.py
@@ -205,3 +205,35 @@ class TestMocoGPTCli:
                 messages=[{"role": "user", "content": "Organization and Project"}]
             )
             assert response.choices[0].message.content == "How can I assist you?"
+
+    def test_should_reply_content_for_specified_tools(self, client: OpenAI):
+        with self.run_service("chat_completions_config.json", "12306"):
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo-1106",
+                messages=[{"role": "user", "content": "Hi, tools"}],
+                tools=[
+                    {
+                        "type": "function",
+                        "function": {
+                            "name": "get_current_weather",
+                            "description": "Get the current weather in a given location",
+                            "parameters": {
+                                "type": "object",
+                                "properties": {
+                                    "location": {
+                                        "type": "string",
+                                        "description": "The city and state, e.g. San Francisco, CA"
+                                    },
+                                    "unit": {
+                                        "type": "string",
+                                        "enum": ["celsius", "fahrenheit"]
+                                    }
+                                },
+                                "required": ["location"]
+                            }
+                        }
+                    }
+                ]
+            )
+
+            assert response.choices[0].message.content == "Hi, tools"
